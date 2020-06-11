18fcdf8004a7d3d6564c518e74ae4b168604300e
ItalianNightClub <65833898+ItalianNightClub@users.noreply.github.com>
2020-06-11 16:55:36 -0400

    Update DeployAgentPlugin.ps1

--- PowerShell-SWIS/DeployAgentPlugin.ps1
+++ PowerShell-SWIS/DeployAgentPlugin.ps1
@@ -1,5 +1,6 @@
 # Get help here:
 # https://github.com/solarwinds/OrionSDK/wiki/PowerShell
+
 # If this is a new env to Orion SDK + PowerShell:
 # 1) Get OrionSDK / SWQL Studio
 # 2) Get the module installed: Install-Module -Name SwisPowerShell
@@ -17,15 +18,15 @@ $username = "Weston"
 $password = " "
 $swis = Connect-Swis -Username $username -Password $password
 
-$Agents = @(1)
+$AgentId = @(3)
 $PluginId = "JobEngine"
 
-foreach ($agent in $Agents) {
-    Invoke-SwisVerb $swis Orion.AgentManagement.Agent UninstallPlugin @(
-        $AgentId
-        $Agent
+foreach ($agent in $AgentId) {
+    Invoke-SwisVerb $swis Orion.AgentManagement.Agent DeployPlugin @(
+    $agent,` 
+    $PluginId
     )
-    Write-Host $PluginId "plugin installed on AgentId"$Agent
+    Write-Host $PluginId "plugin installed on AgentId"$agent
 }
 
 
